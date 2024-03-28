67c95d5fdf8752d111f00ac96b1063ed12957eea
pbillingsby <45179070+PBillingsby@users.noreply.github.com>
2024-03-28 13:50:26 -0400

    add optional tokenAdditionalMetadata

--- src/helpers.ts
+++ src/helpers.ts
@@ -13,7 +13,7 @@ export interface CreateNFTInputs {
   tokenName: string;
   tokenSymbol: string;
   tokenUri: string;
-  tokenAdditionalMetadata: Record<string, string>;
+  tokenAdditionalMetadata?: Record<string, string>;
 }
 
 export interface UploadOffChainMetadataInputs {
@@ -21,7 +21,7 @@ export interface UploadOffChainMetadataInputs {
   tokenSymbol: string;
   tokenDescription: string;
   tokenExternalUrl: string;
-  tokenAdditionalMetadata: Record<string, string>;
+  tokenAdditionalMetadata?: Record<string, string>;
   imagePath: string;
 }
 
@@ -50,7 +50,7 @@ export async function uploadOffChainMetadata(inputs: UploadOffChainMetadataInput
     description: tokenDescription,
     external_url: tokenExternalUrl,
     image: formatIPFSUrl(imageUrl),
-    attributes: Object.entries(tokenAdditionalMetadata).map(([trait_type, value]) => ({ trait_type, value })),
+    attributes: Object.entries(tokenAdditionalMetadata || []).map(([trait_type, value]) => ({ trait_type, value })),
   }
 
   const metadataBlob = new Blob([JSON.stringify(metadata)], { type: 'application/json' })
