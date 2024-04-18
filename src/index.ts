b8208cb5832237af4e51a20f716b0f5a5625dffc
Coach Chuck <christian.krueger.health@gmail.com>
2024-04-18 09:41:05 -0600

    esrun added

--- src/index.ts
+++ src/index.ts
@@ -5,45 +5,35 @@ import { uploadOffChainMetadata } from './helpers';
 import dotenv from 'dotenv';
 dotenv.config();
 
-(async () => {
-  const connection = new Connection(clusterApiUrl('devnet'), 'finalized');
-  const payer = await initializeKeypair(connection, {keypairPath: 'your/path/to/keypair.json'});
+const connection = new Connection(clusterApiUrl('devnet'), 'finalized');
+const payer = await initializeKeypair(connection, {keypairPath: 'your/path/to/keypair.json'});
 
-  const imagePath = 'src/cat.png';
-  const metadataPath = 'src/temp.json';
-  const tokenName = 'Cat NFT';
-  const tokenDescription = 'This is a cat';
-  const tokenSymbol = 'EMB';
-  const tokenExternalUrl = 'https://solana.com/';
-  const tokenAdditionalMetadata = {
-    species: 'Cat',
-    breed: 'Cool',
-  }
+const imagePath = 'src/cat.png';
+const metadataPath = 'src/temp.json';
+const tokenName = 'Cat NFT';
+const tokenDescription = 'This is a cat';
+const tokenSymbol = 'EMB';
+const tokenExternalUrl = 'https://solana.com/';
+const tokenAdditionalMetadata = {
+  species: 'Cat',
+  breed: 'Cool',
+}
 
-  const tokenUri = await uploadOffChainMetadata({
-    tokenName,
-    tokenDescription,
-    tokenSymbol,
-    imagePath,
-    metadataPath,
-    tokenExternalUrl,
-    tokenAdditionalMetadata,
-  }, payer);
+const tokenUri = await uploadOffChainMetadata({
+  tokenName,
+  tokenDescription,
+  tokenSymbol,
+  imagePath,
+  metadataPath,
+  tokenExternalUrl,
+  tokenAdditionalMetadata,
+}, payer);
 
-  await createNFTWithEmbeddedMetadata({
-    payer,
-    connection,
-    tokenName,
-    tokenSymbol,
-    tokenUri,
-    tokenAdditionalMetadata
-  });
-})()
-  .then(() => {
-    console.log('Finished successfully');
-    process.exit(0);
-  })
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+await createNFTWithEmbeddedMetadata({
+  payer,
+  connection,
+  tokenName,
+  tokenSymbol,
+  tokenUri,
+  tokenAdditionalMetadata
+});
